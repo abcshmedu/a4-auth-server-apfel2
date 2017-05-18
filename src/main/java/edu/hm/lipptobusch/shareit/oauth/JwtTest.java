4eede7def2558a4bba29da2698f529abbfdbae4a
mountainflo <tobusch@hm.edu>
2017-05-18 17:48:46 +0200

    add test for generating and validating JWT

---
+++ src/main/java/edu/hm/lipptobusch/shareit/oauth/JwtTest.java
@@ -0,0 +1,57 @@
+package edu.hm.lipptobusch.shareit.oauth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by floriantobusch on 18.05.17.
+ */
+public class JwtTest {
+    public static void main(String[] args) {
+
+        /**
+         * Example with JWT from: https://stormpath.com/blog/beginners-guide-jwts-in-java
+         */
+
+        //generate new JWT
+        try {
+            Date expirationDate = new Date(System.currentTimeMillis() + 60000);
+
+            String jwt = Jwts.builder()
+                    .setSubject("Reply to Access-Token")
+                    .setExpiration(expirationDate)
+                    .claim("name", "Robert Token Man") //claims contain the body elements
+                    .claim("scope", "self groups/admins")
+                    .signWith(
+                            SignatureAlgorithm.HS256, "secret".getBytes("UTF-8")
+                    )
+                    .compact();
+
+
+
+            //validating JWT
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey("secret".getBytes("UTF-8"))
+                    .parseClaimsJws(jwt);
+            String scope = (String) claims.getBody().get("scope");
+            assertEquals(scope, "self groups/admins");
+
+
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+    }
+}
